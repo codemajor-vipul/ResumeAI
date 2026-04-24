eade96b755a4c3ee13f6c57304f9fe09fbdc7d3e
Vipul Agarwal <vipul2497@gmail.com>
2026-04-24 23:34:46 +0530

    changes

--- src/ResumeAI.Auth.API/Data/Migrations/20260424151334_InitialAuthAPI.Designer.cs
+++ src/ResumeAI.Auth.API/Data/Migrations/20260424151334_InitialAuthAPI.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ResumeAI.Auth.API.Data;
@@ -11,9 +12,11 @@ using ResumeAI.Auth.API.Data;
 namespace ResumeAI.Auth.API.Data.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260424151334_InitialAuthAPI")]
+    partial class InitialAuthAPI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
